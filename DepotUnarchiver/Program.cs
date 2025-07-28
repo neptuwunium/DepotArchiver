cd7d3f6f7000ef23ccdf067183087ceba0a0de74
Ada <yretenai@gmail.com>
2025-07-28 23:11:39 +0100

    unarchiver: process app ids, multi depot extract

--- DepotUnarchiver/Program.cs
+++ DepotUnarchiver/Program.cs
@@ -20,22 +20,120 @@ internal static class Program {
 
 		var flags = ProgramFlags.Instance;
 
-		var depotPath = Path.Combine(Path.GetFullPath(flags.DepotDirectory), flags.DepotId.ToString("D", CultureInfo.InvariantCulture));
-		var depotKeyPath = Path.Combine(Path.GetFullPath(flags.DepotDirectory), flags.DepotId.ToString("D", CultureInfo.InvariantCulture) + ".depotkey");
-		var manifestsPath = Path.Combine(depotPath, "manifest");
+		var appDepotPlan = ParsePlan(flags);
+		var depotPlan = new Dictionary<uint, HashSet<ulong>>();
 
-		if (flags.ManifestIds.Count == 0) {
-			foreach (var manifestId in Directory.EnumerateFiles(manifestsPath, "*", SearchOption.TopDirectoryOnly)) {
-				flags.ManifestIds.Add(ulong.Parse(Path.GetFileName(manifestId), NumberStyles.Integer));
+		foreach (var (id, manifestIds) in appDepotPlan) {
+			if (manifestIds.Count > 0) {
+				depotPlan[id] = manifestIds;
+				continue;
+			}
+
+			var idStr = id.ToString("D", CultureInfo.InvariantCulture);
+			var appPath = Path.Combine(Path.GetFullPath(flags.DepotDirectory), idStr + ".vdf");
+
+			if (!File.Exists(appPath)) {
+				depotPlan[id] = manifestIds;
+				continue;
+			}
+
+			var app = KeyValue.LoadAsText(appPath);
+			if (app == null) {
+				continue;
+			}
+
+			foreach (var depot in app.Children.Where(x => x.Name == "depots").FirstOrDefault(KeyValue.Invalid).Children) {
+				var manifests = depot["manifests"];
+
+				if (manifests.Children.Count == 0) {
+					continue;
+				}
+
+				foreach (var branch in manifests.Children) {
+					var gid = branch["gid"];
+
+					if (string.IsNullOrEmpty(gid.Value) || string.IsNullOrEmpty(depot.Name)) {
+						continue;
+					}
+
+					var depotId = uint.Parse(depot.Name);
+					var manifestId = ulong.Parse(gid.Value);
+					if (!depotPlan.TryGetValue(depotId, out var depotManifests)) {
+						depotPlan[depotId] = depotManifests = [];
+					}
+
+					depotManifests.Add(manifestId);
+				}
 			}
 		}
 
-		foreach (var manifestId in flags.ManifestIds) {
-			ProcessManifest(manifestsPath, manifestId, depotKeyPath, flags, depotPath);
+		foreach (var (depotId, manifestIds) in depotPlan) {
+			var depotIdStr = depotId.ToString("D", CultureInfo.InvariantCulture);
+			var depotPath = Path.Combine(Path.GetFullPath(flags.DepotDirectory), depotIdStr);
+			var depotKeyPath = Path.Combine(Path.GetFullPath(flags.DepotDirectory), depotIdStr + ".depotkey");
+			var manifestsPath = Path.Combine(depotPath, "manifest");
+
+			if (manifestIds.Count == 0) {
+				foreach (var manifestId in Directory.EnumerateFiles(manifestsPath, "*", SearchOption.TopDirectoryOnly)) {
+					manifestIds.Add(ulong.Parse(Path.GetFileName(manifestId), NumberStyles.Integer));
+				}
+			}
+
+			var targetDirectory = flags.TargetDirectory;
+			if (flags.AppendDepot) {
+				targetDirectory = Path.Combine(targetDirectory, depotIdStr);
+			}
+
+			foreach (var manifestId in manifestIds) {
+				ProcessManifest(flags, targetDirectory, manifestsPath, manifestId, depotKeyPath, depotPath);
+			}
 		}
 	}
 
-	private static void ProcessManifest(string manifestsPath, ulong manifestId, string depotKeyPath, ProgramFlags flags, string depotPath) {
+	private static Dictionary<uint, HashSet<ulong>> ParsePlan(ProgramFlags flags) {
+		var result = new Dictionary<uint, HashSet<ulong>>();
+		var lastDepotId = flags.DepotId.Contains(':', StringComparison.CurrentCulture)
+			? ParseDepotManifest(result, flags.DepotId, 0)
+			: uint.Parse(flags.DepotId, NumberStyles.Integer, CultureInfo.InvariantCulture);
+
+		result[lastDepotId] = [];
+
+		_ = flags.ManifestIds.Aggregate(lastDepotId, (current, manifestId) => ParseDepotManifest(result, manifestId, current));
+
+		return result;
+	}
+
+	private static uint ParseDepotManifest(Dictionary<uint, HashSet<ulong>> result, string value, uint lastDepotId) {
+		Span<Range> ranges = stackalloc Range[2];
+		Span<string> separators = [":"];
+		var chars = value.AsSpan();
+		var n = chars.SplitAny(ranges, separators);
+		ulong manifestId;
+		if (n == 2) {
+			lastDepotId = uint.Parse(chars[ranges[0]], NumberStyles.Integer, CultureInfo.InvariantCulture);
+			manifestId = ranges[1].GetOffsetAndLength(chars.Length).Length == 0
+				? 0
+				: ulong.Parse(chars[ranges[1]], NumberStyles.Integer, CultureInfo.InvariantCulture);
+		} else {
+			if (lastDepotId == 0) {
+				return lastDepotId;
+			}
+
+			manifestId = ulong.Parse(value, NumberStyles.Integer, CultureInfo.InvariantCulture);
+		}
+
+		if (!result.TryGetValue(lastDepotId, out var manifests)) {
+			result[lastDepotId] = manifests = [];
+		}
+
+		if (manifestId > 0) {
+			manifests.Add(manifestId);
+		}
+
+		return lastDepotId;
+	}
+
+	private static void ProcessManifest(ProgramFlags flags, string targetDirectory, string manifestsPath, ulong manifestId, string depotKeyPath, string depotPath) {
 		var manifestIdStr = manifestId.ToString("D", CultureInfo.InvariantCulture);
 		var manifestPath = Path.Combine(manifestsPath, manifestIdStr);
 
@@ -68,7 +166,10 @@ internal static class Program {
 		var fileMaps = new List<MemoryMappedFile>();
 		var ops = new List<ChunkLoadOp>();
 
-		var targetDirectory = flags.AppendManifest ? Path.Combine(flags.TargetDirectory, manifestIdStr) : flags.TargetDirectory;
+		if (flags.AppendManifest) {
+			targetDirectory = Path.Combine(targetDirectory, manifestIdStr);
+		}
+
 		var sum = 0UL;
 
 		foreach (var file in manifest.Files.Where(file => flags.Filter.Count == 0 || flags.Filter.Any(x => x.IsMatch(file.FileName)))) {
@@ -178,6 +279,5 @@ internal static class Program {
 		}
 	}
 
-	// todo: make MemoryMappedFile a list so the chunk is decompressed only once.
 	private record ChunkLoadOp(MemoryMappedFile File, DepotManifest.ChunkData Chunk, string ChunkPath, byte[] DepotKey);
 }
--- DepotUnarchiver/ProgramFlags.cs
+++ DepotUnarchiver/ProgramFlags.cs
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 using System.Globalization;
+using System.Reflection;
 using System.Text.RegularExpressions;
 using DragonLib.CommandLine;
 
@@ -23,11 +24,11 @@ internal record ProgramFlags : CommandLineFlags {
 	[Flag("filter", Help = "only export files that match these regex filters", FileListPrefix = '@')]
 	public List<Regex> Filter { get; set; } = [];
 
-	[Flag("depot-id", Positional = 0, Help = "the depot id to unarchive", Extra = NumberStyles.Integer, IsRequired = true)]
-	public uint DepotId { get; set; }
+	[Flag("depot-id", Positional = 0, Help = "the depot id to unarchive", IsRequired = true)]
+	public string DepotId { get; set; } = string.Empty;
 
-	[Flag("manifest-id", Positional = 1, Help = "the manifest id to unarchive", Extra = NumberStyles.Integer)]
-	public HashSet<ulong> ManifestIds { get; set; } = [];
+	[Flag("manifest-id", Positional = 1, Help = "the manifest id to unarchive")]
+	public HashSet<string> ManifestIds { get; set; } = [];
 
 	[Flag("threads", Help = "number of threads to spawn")]
 	public int Threads { get; set; } = Environment.ProcessorCount;
@@ -35,6 +36,24 @@ internal record ProgramFlags : CommandLineFlags {
 	[Flag("append-manifest-id", Help = "Append the manifest id to the file path")]
 	public bool AppendManifest { get; set; }
 
+	[Flag("append-depot-id", Help = "Append the depot id to the file path")]
+	public bool AppendDepot { get; set; }
+
 	[Flag("no-clobber", Aliases = ["n"], Help = "Do not overwrite files that already exist")]
 	public bool NoClobber { get; set; }
+
+	private static void PrintHelp(Dictionary<PropertyInfo, (FlagAttribute Flag, Type FlagType)> flags, object instance, CommandLineOptions options, bool helpInvoked) {
+		CommandLineFlagsParser.PrintHelp(flags, instance, options, helpInvoked);
+		Console.WriteLine("Id Format:");
+		Console.WriteLine("\tBoth depot-id and manifest-id can be depotId:manifestId.");
+		Console.WriteLine("\tThe first id argument will always be depotId.");
+		Console.WriteLine("\tDepot ID can be an App ID, which will pull in all depots for the app if a vdf is cached.");
+		Console.WriteLine("\tIf there is no :, every subsequent id will be considered a manifest id for the last used depot id.");
+		Console.WriteLine("\texample: 440 7561350075549843378 441:6322590814356637464 2849445313493516261 220:0");
+		Console.WriteLine("\twill process the following manifests:");
+		Console.WriteLine("\tdepot 440, manifest 7561350075549843378");
+		Console.WriteLine("\tdepot 441, manifest 6322590814356637464");
+		Console.WriteLine("\tdepot 441, manifest 2849445313493516261");
+		Console.WriteLine("\tdepot 233, manifest 1358845133936836982");
+	}
 }
