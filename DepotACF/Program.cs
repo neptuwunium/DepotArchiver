a1ec25efc47b627b874d8cf34a2a68b47a591952
Ada <ada@chronovore.dev>
2025-12-26 16:51:35 +0530

    acf: begin work on DepotACF
    a tool to unarchive Depots as if the client did it

---
+++ DepotACF/Program.cs
@@ -0,0 +1,11 @@
+﻿// SPDX-FileCopyrightText: 2025 Legiayayana
+//
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+namespace DepotACF;
+
+class Program {
+	static void Main(string[] args) {
+		Console.WriteLine("Hello, World!");
+	}
+}
